cbcd864804afe9bb711a8007b340567f1d905a5a
jn2626 <jyrkinieminen91@gmail.com>
2026-04-11 00:01:55 +0200

    Update vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
-      exclude: ['src/.internal/**']
+      exclude: ['src/.internal/**'],
+      reporter: ['text', 'lcov']
     }
   }
 })
